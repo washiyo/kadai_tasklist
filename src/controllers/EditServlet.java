09c0d841a29e3d014be0c69e3272db740f459086
hiroshi.nakagawa <bzhongchuan439@gmail.com>
2019-07-31 14:39:08 +0900

    Add EditServlet

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -34,13 +34,17 @@ public class EditServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
+        // 該当のIDのメッセージ1件のみをデータベースから取得
         Task m = em.find(Task.class, Integer.parseInt(request.getParameter("id")));
+
         em.close();
 
+        // メッセージ情報とセッションIDをリクエストスコープに登録
         request.setAttribute("task", m);
         request.setAttribute("_token", request.getSession().getId());
 
-        request.getSession().setAttribute("message_id", m.getId());
+        // メッセージIDをセッションスコープに登録
+        request.getSession().setAttribute("task_id", m.getId());
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
         rd.forward(request, response);
